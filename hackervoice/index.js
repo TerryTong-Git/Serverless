bfd22c1725a9977ee23c3ed06eec2a62bf7aa71d
terry tong <dooshtheboss1@gmail.com>
2022-06-29 18:26:54 +0800

    hackervoice

---
+++ hackervoice/index.js
@@ -0,0 +1,7 @@
+module.exports = async function (context, req) {
+    password = req.query.password
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: password
+    };
+}
