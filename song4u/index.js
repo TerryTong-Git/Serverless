5c6870871a32cc3f9983a1a0a29d9afd9786e239
terry tong <dooshtheboss1@gmail.com>
2022-07-20 15:25:34 +0800

    done

--- song4u/index.js
+++ song4u/index.js
@@ -2,10 +2,10 @@
 const querystring = require('qs');
 const fetch = require('node-fetch')
 module.exports = async function(context,req) {
-    const queryObject = querystring.parse(req.body);
-    const Body = queryObject.Body
+    // const queryObject = querystring.parse(req.body);
+    // const Body = queryObject.Body
     // const url = Body.split("")[2]
-    // let Body = req.body.entry
+    let Body = req.body.entry
     console.log(Body)
     let resp = await fetch(Body, {
         method:"GET"
@@ -15,21 +15,34 @@ module.exports = async function(context,req) {
     console.log(result)
     let img = result[0].faceAttributes.age
     
+    const songs = {"GenZ":"https://open.spotify.com/track/0SIAFU49FFHwR3QnT5Jx0k?si=1c12067c9f2b4fbf", 
+"GenY":"https://open.spotify.com/track/1Je1IMUlBXcx1Fz0WE7oPT?si=a04bbdf6ec4948b9", 
+"GenX":"https://open.spotify.com/track/4Zau4QvgyxWiWQ5KQrwL43?si=790d9e3ef2ed408d", 
+"BabyBoomers":"https://open.spotify.com/track/4gphxUgq0JSFv2BCLhNDiE?si=1abb329f2dc24f50", 
+"Unknown":"https://open.spotify.com/track/5ygDXis42ncn6kYG14lEVG?si=84b49b41d09d4d11"}
+
     if (5 < img && img < 25){
-        age = "GenZ"}
+        age = "GenZ";
+        value = songs[age]
+        }
     else if 
     	(24 < img && img < 41) {
             age = "GenY"
+            value = songs[age]
     }
     else if (40<img && img<57){
         age = "GenX"
+        value = songs[age]
     } // need the and or else stop on the first true with the inequality sign, act kinda like or
     else if (57<img && img<76) {
         age = "BabyBoomers"
+        value = songs[age]
     }
 
+    let return_string = `We guessed you're part of this generation: ${age}! Happy listening to ${value}`
+
     context.res = {
-        body: age
+        body: return_string
     };
 }
 
