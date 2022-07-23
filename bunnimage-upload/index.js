385445802491cba2e6728df055761e111fe29fbc
terry tong <dooshtheboss1@gmail.com>
2022-07-23 17:50:53 +0800

    added

--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -1,13 +1,31 @@
+const multipart = require('parse-multipart')
+const fetch = require("node-fetch")
+const {BlobServiceClient} = require("@azure/storage-blob")
+const connectionstring = process.env["AZURE_STORAGE_CONNECTION_STRING"]
+
 module.exports = async function (context, req) {
+    
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+    const boundary = multipart.getBoundary(req.headers['content-type'])
+    const body = req.body
+    const parseBody = multipart.Parse(body,boundary)
+    let exted = parseBody[0].type.split("/")[1]
+    let responseMessage = await Uploadfile(parseBody, exted)
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
+}
+
+async function Uploadfile(parsedbody, ext) {
+    const blobServicesClient = BlobServiceClient.fromConnectionString(connectionstring)
+    const containername = "serverlessblob1"
+    const containerclient = blobServicesClient.getContainerClient(containername)
+
+    const blobName = "text." + ext;
+    const blockblobclient  = containerclient.getBlockBlobClient(blobName)
+
+    const uploadblobResponse = await blockblobclient.upload(parsedbody[0].data, parsedbody[0].data.length)
+    return "File Saved"
 }
